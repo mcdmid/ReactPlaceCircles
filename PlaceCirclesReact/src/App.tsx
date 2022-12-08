447a5e1297ece7196bd72fa60c073b1989b7065e
mcdmid <cameronmcdermid@live.com>
2022-12-08 15:43:15 +0000

    Getting click coords on the React components hooks.

--- PlaceCirclesReact/src/App.tsx
+++ PlaceCirclesReact/src/App.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+type TPoint = {
+  x: number; 
+  y: number;
+};
+
 function App() {
-  function handlePlaceCircle(e: React.MouseEvent) {}
+  const [points, setPoints] = useState<TPoint[]>([]);
+
+  function handlePlaceCircle(e: React.MouseEvent<HTMLDivElement>) {
+    //console.log(e);
+    const { clientX, clientY } = e;
+    setPoints([
+      ...points,
+      {
+        x: clientX,
+        y: clientY,
+      },
+    ]);
+  }
 
   return <div className="App" onClick={handlePlaceCircle}></div>;
 } 
