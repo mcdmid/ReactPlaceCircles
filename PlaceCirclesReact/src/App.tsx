e8364edac0a463af4cbc30d85bc2d9e0c0572aa8
mcdmid <cameronmcdermid@live.com>
2022-12-08 11:46:26 +0000

    Initialised react ts project.

---
+++ PlaceCirclesReact/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  function handlePlaceCircle(e: React.MouseEvent) {}
+
+  return <div className="App" onClick={handlePlaceCircle}></div>;
+} 
+
+export default App;
